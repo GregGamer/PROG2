9b415de936946773af147c86da8dd5168db13a38
GregGamer <gregamer98@gmail.com>
2021-03-15 13:48:27 +0100

    compiling first programm

--- Übung1/euro2money.c
+++ Übung1/euro2money.c
@@ -2,7 +2,14 @@
 #include <stdlib.h>
 
 void euro2money(int euro){
-    printf("%i\n",euro);
+    printf("Eingegebener Eurobetrag: %i\n",euro);
+
+    if(euro >= 500){
+        int euro500 = euro/500;
+        printf("500 Euroscheine: %i\n",euro500);
+    }
+    euro = euro%500;
+
     
 }
 
