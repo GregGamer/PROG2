1cc8bbbafd5d9cb4891b381fdd88ae95b379da8d
GregGamer <gregamer98@gmail.com>
2021-03-16 15:32:52 +0100

    changing files

--- Übung1/WaG-bsp1.c
+++ Übung1/WaG-bsp1.c
@@ -1,28 +1,22 @@
+//Gregor Wagner
+//WaG-bsp1.c
+
 #include <stdio.h>
-#include <stdlib.h>
 
-void euro2money(int euro){
+int main (void){
     int euros[] = {500,200,100,50,20,10,5,2,1};
+    int input = 0; 
+    
+    scanf("Bitte den Euro Betrag eingeben: %d",&input);
 
-    printf("Eingegebener Eurobetrag: %d\n",euro);
-
-    for (int i = 0; i<sizeof(euros);i++){ 
-        if(euro<1){
-            return;
+    for(int i = 0; i<(int)sizeof(euros);++i){
+        if (input < 0){
+            return 0;
         }
 
-        if(euro >= euros[i]){
-            int help = euro/euros[i];
-            printf("%d Euroscheine: %d\n",euros[i],help);
-        }
-        euro = euro%euros[i];
+        printf("%d€: %d",euros[i],input/euros[i]);
 
-    } 
+        input = input % euros[i];
+    }
 
-}
-
-int main (int argc, char* argv[]){
-    int euros = 12345;
-    euro2money(euros);
-    return 0;
 }
