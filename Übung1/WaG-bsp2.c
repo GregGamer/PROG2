20ce9048df242dee253444dcb92b4922df39c861
GregGamer <gregamer98@gmail.com>
2021-03-16 16:11:22 +0100

    finish bsp 2

--- Übung1/WaG-bsp2.c
+++ Übung1/WaG-bsp2.c
@@ -1,1 +1,33 @@
 //Gregor Wagner
+//WaG-bsp2.c
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main (void) {
+    // Variables
+    int x1,y1;
+    int x2,y2;
+
+    //get the input
+    printf("Input x1: ");
+    scanf("%d",&x1);
+    printf("Input y1: ");
+    scanf("%d",&y1);
+    printf("Input x2: ");
+    scanf("%d",&x2);
+    printf("Input y2: ");
+    scanf("%d",&y2);
+
+    //Berechnung
+    int x,y;
+    x = x2-x1;
+    y = y2-y1;
+    double erg = sqrt(x*x+y*y);
+
+    //Output
+    printf("Distance between the said points: %f\n",erg);
+
+    return 0;
+}
