3ddd34bf4a35c21657e2ab63ff55dbe1fe7a6164
GregGamer <gregamer98@gmail.com>
2021-03-18 01:29:48 +0100

    roboter kann moven, gibt aber noch einen koordinaten fehler

--- Übung2/WaG-game1.c
+++ Übung2/WaG-game1.c
@@ -3,22 +3,32 @@
 
 //include
 #include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
 //globale constanten
+const int IS_NUMMER = 8;
 const int GAMEFIELD_WIDTH = 8;
 const int GAMEFIELD_HEIGHT = 8;
 
 //prototypen
 void initGamefield(int WaG_Gamefield[GAMEFIELD_WIDTH][GAMEFIELD_HEIGHT]);
-void printGamefield(int WaG_Gamefield[GAMEFIELD_WIDTH][GAMEFIELD_HEIGHT]);
+void WaG_PrintGame(int WaG_Gamefield[GAMEFIELD_WIDTH][GAMEFIELD_HEIGHT], int roboterPosition[]);
+void WaG_CreateObstacles(int WaG_Gamefield[GAMEFIELD_WIDTH][GAMEFIELD_HEIGHT], int n);
+void WaG_MoveOneStep(int WaG_Gamefield[GAMEFIELD_WIDTH][GAMEFIELD_HEIGHT], int roboterPosition[]);
+void WaG_StepByStep(int WaG_Gamefield[GAMEFIELD_WIDTH][GAMEFIELD_HEIGHT], int roboterPosition[]);
 
 int main (void) {
     //variablen
     int WaG_Gamefield[GAMEFIELD_HEIGHT][GAMEFIELD_WIDTH];
+    int roboterPosition[] = {0,7,IS_NUMMER % 4};
+    int anzObstacles = 5;
 
     initGamefield(WaG_Gamefield);
-    printGamefield(WaG_Gamefield);
-
+    WaG_CreateObstacles(WaG_Gamefield, anzObstacles);
+    WaG_StepByStep(WaG_Gamefield,roboterPosition);
 
     return 0;
 }
@@ -31,19 +41,72 @@ void initGamefield (int WaG_Gamefield[GAMEFIELD_WIDTH][GAMEFIELD_HEIGHT]) {
     }
 }
 
-void printGamefield (int WaG_Gamefield[GAMEFIELD_WIDTH][GAMEFIELD_HEIGHT]) {
-    for(int i = 0; i<GAMEFIELD_WIDTH; ++i){
-        for (int j = 0; j < GAMEFIELD_HEIGHT; ++j){
+void WaG_PrintGame (int WaG_Gamefield[GAMEFIELD_WIDTH][GAMEFIELD_HEIGHT], int roboterPosition[]) {
+
+//    system("@cls||clear");
+    
+    for(int i = 0; i < GAMEFIELD_HEIGHT; ++i){
+        for (int j = 0; j < GAMEFIELD_WIDTH; ++j){
+
+            if(roboterPosition[0] == i && roboterPosition[1] == j){
+                printf("0");
+                continue;
+            }
+
             if (WaG_Gamefield[i][j] == 0){
                 printf(".");
             }
             else if (WaG_Gamefield[i][j] == 1) {
                 printf("#");
             }
-            else if (WaG_Gamefield[i][j] == 2) {
-                printf("0");
-            }
+
         }
         printf("\n");
+    }
+}
+
+void WaG_CreateObstacles(int WaG_Gamefield[GAMEFIELD_WIDTH][GAMEFIELD_HEIGHT],int n){
+    int Xrand;
+    int Yrand;
+    time_t t;
+
+    srand((unsigned) time(&t));
+
+    for (int i = 0; i < n; ++i){
+        Xrand = rand() % GAMEFIELD_WIDTH;
+        Yrand = rand() % GAMEFIELD_HEIGHT;
+        WaG_Gamefield[Xrand][Yrand] = 1;
+    }
+}
+
+void WaG_MoveOneStep(int WaG_Gamefield[GAMEFIELD_WIDTH][GAMEFIELD_HEIGHT], int roboterPosition[]){
+    int nextStep[2];
+    nextStep[0] = roboterPosition[0] + ((roboterPosition[2] == 1) * 1 + (roboterPosition[2] == 3 * -1));
+    nextStep[1] = roboterPosition[1] + ((roboterPosition[2] == 2) * 1 + (roboterPosition[2] == 0 * -1));
+
+    //Berechnet mir die neue Blickrichtung wenn vor dem Bot ein Hinderniss ist
+    roboterPosition[2] = (WaG_Gamefield[nextStep[0]][nextStep[1]] == 1) * \
+                         (((IS_NUMMER/5) * 1 + !(IS_NUMMER/5) * -1 + roboterPosition[2]) % 4); 
+
+    nextStep[0] = roboterPosition[0] + ((roboterPosition[2] == 1) * 1 + (roboterPosition[2] == 3 * -1));
+    nextStep[1] = roboterPosition[1] + ((roboterPosition[2] == 2) * 1 + (roboterPosition[2] == 0 * -1));
+
+    roboterPosition[0] = nextStep[0] % GAMEFIELD_WIDTH;
+    roboterPosition[1] = nextStep[1] % GAMEFIELD_HEIGHT;
+}
+
+void WaG_StepByStep(int WaG_Gamefield[GAMEFIELD_WIDTH][GAMEFIELD_HEIGHT], int roboterPosition[]){
+    //variablen
+    char input[10] = "";
+
+    WaG_PrintGame(WaG_Gamefield,roboterPosition);
+        
+    while (strcmp(input,"quit")){
+        //input
+        printf("StepByStep Mode, quit by typing quit: ");
+        scanf("%s",input);
+        
+        WaG_MoveOneStep(WaG_Gamefield, roboterPosition);
+        WaG_PrintGame(WaG_Gamefield, roboterPosition);
     }
 }
---
+++ Übung2/WaG-game2.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main (void){
+    printf("%d",0%4);
+    printf("%d",-1%4);
+}
