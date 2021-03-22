7adccc6877b0eec649ffd9c94bf574fade729523
GregGamer <gregamer98@gmail.com>
2021-03-22 10:06:07 +0100

    funktionen angelegt

--- Übung3/WaG-vec2.c
+++ Übung3/WaG-vec2.c
@@ -8,9 +8,44 @@
 //Makros und globale Constanten
 
 //Prototypen
+const char *strcichr (const char *s, char c);                   //strchr
+const char *strrcichr (const char *s, char c);                   //strrchar
+const char *strcipbrk (const char *s, const char *accapt);      //strpbrk
 
 int main (void) {
+    //variablen
+    char str[20] = "Hallo Welt!\n";
+    char c = " ";
+    char result[5];
+
+    result = strcichr(str,c);
+    printf("%s",result);
+
+    result = strrcichr(str,c);
+    printf("%s",result);
+
+    result = strcipbrk(str,c);
+    printf("%s",result);
 
 
     return 0;
+}
+
+
+const char *strcichr (const char *s, char c){                   //strchr
+
+    return NULL;
+}
+
+
+const char *strrcichr (const char *s, char c){                   //strchr
+
+    return NULL;
+}
+
+
+const char *strcipbrk (const char *s, const char *accapt){      //strpbrk
+
+
+    return NULL;
 }
